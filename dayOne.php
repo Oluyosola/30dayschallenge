7f7af43dbe9b36c95f548c75341931f120b52123
Oluyosola <elizabeth.afolabi@techadvance.ng>
2020-03-27 14:49:47 +0100

    dayOne challenge solved

---
+++ dayOne.php
@@ -0,0 +1,11 @@
+<?php
+//a program that tests if a number is positive or negative
+function testNumber($y){
+    if ($y>0){
+        echo "it is a positive number";//prints if it is a positive number
+    }
+    else 
+    echo "it is a negative number";//prints if it is a negative number
+}
+testNumber(-2);
+?>
